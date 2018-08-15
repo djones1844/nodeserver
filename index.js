b0f18b00443f6e04f1da8c73871943bf0d228a5c
Daniel Jones <djones1844@gmail.com>
2018-08-15 16:45:20 -0700

    added headers and payload support

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  // Dependencies
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // server should respond to requests with a string
 const server = http.createServer((req, res) => {
@@ -13,18 +14,36 @@ const server = http.createServer((req, res) => {
   // get url and parse it
   let parseUrl = url.parse(req.url, true)
 
-  // get the path from the url
+  // get the path 
   let path = parseUrl.pathname
   let trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
-  // send the response
-  res.end('Hello World\n')
+  // Get the query string as an object
+  let queryStringObject = parseUrl.query
 
-  // log the request
-  console.log('Request received on path: '+trimmedPath)
+  // get the HTTP Method
+  let method = req.method.toLowerCase()
 
+  // get the headers as an object
+  let headers = req.headers 
+ 
+  // Get Payload, if any
+  let decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  req.on('data', data => buffer += decoder.write(data))
+  req.on('end', () => {
+  	bufer += decoder.end
+   
+    // send the response
+    res.end('Hello World\n')
 
+    // log the request path
+    console.log('Request received with these headers: ', headers)
+
+  })
+  
 })
+
 // start the server and have it listen on port 3000
 server.listen(3000, () => console.log('The server is listening on port 3000 now'))
 
